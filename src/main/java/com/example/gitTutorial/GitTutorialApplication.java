f6bb0c6fdbe4e53be21f05f2a7fd45e82f8c3f59
demoFaiz <158502921+demoFaiz@users.noreply.github.com>
2024-02-04 19:42:15 +0530

    comment removed

--- src/main/java/com/example/gitTutorial/GitTutorialApplication.java
+++ src/main/java/com/example/gitTutorial/GitTutorialApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//comment
 public class GitTutorialApplication {
 
 	public static void main(String[] args) {
